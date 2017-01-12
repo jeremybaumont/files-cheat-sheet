be0cd62a2cf606b3ad4e8c3b04d2c8b61b5e6f69
Jeremy Baumont <jeremy.baumont@gmail.com>
2017-01-12 18:20:18 +1100

    It begins...

--- files.rb
+++ files.rb
@@ -3,6 +3,20 @@ cheatsheet do
     docset_file_name 'files'
     keyword 'files'
     category do
-        id ''
+        id 'filesystem'
+        entry do
+            name 'Check inodes'
+            command 'df -ih /'
+        end
+        entry do 
+            name 'Remove a directory with a lots of files'
+            command 'mkdir empty_dir'
+            command 'rsync -a --delete empty_dir/    yourdirectory/'
+        end
+        entry do
+            name 'Find the directory with a lots of files'
+            command 'find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n'
+        end
+
     end
 end
